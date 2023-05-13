5604f4ee9499f4f2641606e69d2cdc7f9ba48869
ladislav-moravec <118938709+ladislav-moravec@users.noreply.github.com>
2023-05-13 11:08:39 +0200

    .

--- calculator.py
+++ calculator.py
@@ -5,3 +5,24 @@ class Calculator:
 
     def add(self, a, b):
         return a + b
+
+    def reduce(self, a, b):
+        return a - b
+
+    """
+    def div(self, a, b):
+
+        try:
+            if b == 0:
+                print("You can't devide by zero")
+            else:
+                return a / b
+    """
+
+    def div(self, a, b):
+
+        if b == 0:
+            raise ZeroDivisionError("You can't divide by zero")
+        return a / b
+
+
--- test_calculator.py
+++ test_calculator.py
@@ -4,6 +4,7 @@ from calculator import Calculator
 
 
 class TestCalculator(TestCase):
+
     def test_to_the_power_of(self):
         calc = Calculator()
 
@@ -17,3 +18,17 @@ class TestCalculator(TestCase):
         self.assertEqual(4, calc.add(2, 2))
         self.assertEqual(14, calc.add(9, 5))
         self.assertNotEqual(4, calc.add(2, 4))
+
+    def test_reduce(self):
+        calc = Calculator()
+
+        self.assertEqual(4, calc.reduce(6, 2))
+        self.assertEqual(89140, calc.reduce(89150, 10))
+        self.assertNotEqual(4258390, calc.reduce(323, 4895479802))
+
+    def test_div(self):
+        calc = Calculator()
+
+        self.assertEqual(4, calc.div(16, 4))
+        self.assertEqual(2, calc.div(160, 80))
+        self.assertNotEqual(2, calc.div(2324, 424))
