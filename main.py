45be253d8f5807b75ef91fd91c8b28a684481d4e
ladislav-moravec <118938709+ladislav-moravec@users.noreply.github.com>
2023-05-13 14:56:26 +0200

    add main test wiki

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome(executable_path='./chromedriver')
 driver.get('https://wikipedia.org')
 driver.find_element(By.NAME, "search").send_keys("Python", Keys.ENTER)
+#driver.find_element(By.LINK_TEXT, "ABC (programovací jazyk)")
 
 time.sleep(20)
